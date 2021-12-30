36484b6ba6c126809a94f2e3547e07af622298c4
eremophobia <succducc0@gmail.com>
2021-12-30 02:57:24 +0100

    Multiple versions option added.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #Made by kyo :)
 
 #Colors
@@ -8,14 +10,16 @@ MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(prin
 RESETBG="$(printf '\e[0m\n')"
 
 clear
-
-mkdir server1.8.8
-cd server1.8.8
-
 sleep 1
 
-echo -e "${RED}Installation will require some time, sit back and make a coffee."
-echo -e "${YELLOW}DO NOT STOP THIS SCRIPT!"
+################################################################################################
+
+#User input required (version)
+echo "${YELLOWBG}${BLACK}Which verison do you want to install?"
+read -p "Available versions: 1.8.8 , 1.9.4 , 1.10.2 , 1.11.2 , 1.12.2 .${RESETBG}${CYAN} " input
+
+		echo -e "${RED}Installation will require some time, sit back and make a coffee."
+		echo -e "${YELLOW}DO NOT STOP THIS SCRIPT!"
 
 
         echo -e "${GREEN}"
@@ -70,11 +74,34 @@ echo -e "${RED}Done!"
 echo " "
 
 #Installing server jar on server directory
-echo -e "${RED}[${GREEN}#${RED}] Installing server.jar ..."
+echo -e "${RED}[${GREEN}#${RED}] Downloading jar file ..."
 
         echo -e "${CYANBG}${BLACK}"
 
-wget -O server.jar https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar -q --show-progress
+if [ "$input" == "1.8.8" ]; then
+mkdir server1.8.8
+cd server1.8.8
+        wget -O server1.8.8.jar https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar -q --show-progress
+
+elif [ "$input" == "1.9.4" ]; then
+mkdir server1.9.4
+cd server1.9.4
+        wget -O server1.9.4.jar https://papermc.io/api/v2/projects/paper/versions/1.9.4/builds/775/downloads/paper-1.9.4-775.jar -q --show-progress
+
+elif [ "$input" == "1.10.2" ]; then
+mkdir server1.10.2
+cd server1.10.2
+        wget -O server1.10.2.jar https://papermc.io/api/v2/projects/paper/versions/1.10.2/builds/918/downloads/paper-1.10.2-918.jar -q --show-progress
+
+elif [ "$input" == "1.11.2" ]; then
+mkdir server1.11.2
+cd server1.11.2
+        wget -O server1.11.2.jar https://papermc.io/api/v2/projects/paper/versions/1.11.2/builds/1106/downloads/paper-1.11.2-1106.jar -q --show-progress
+
+elif [ "$input" == "1.12.2" ]; then
+mkdir server1.12.2
+cd server1.12.2
+        wget -O server1.12.2.jar https://papermc.io/api/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
 
         echo -e "${RESETBG}${GREEN}"
 
@@ -90,3 +117,7 @@ echo -e "(nano documentation: ${CYAN}https://www.nano-editor.org/docs.php${YELLO
 echo " "
 echo -e "Thanks for using my installation script ${RED}:)"
 echo " "
+
+        else
+                echo -e "${RED}Please answer yes or no.${RESETBG}"
+fi
